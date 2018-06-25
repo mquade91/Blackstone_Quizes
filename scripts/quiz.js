3f33822a383b5d0b35824bd231359682f2c5c7d7
Matthew Quade <quadem09@gmail.com>
2018-06-24 22:45:29 -0400

    working through js styling

---
+++ scripts/quiz.js
@@ -0,0 +1,17 @@
+console.log("script connected")
+
+
+correct = 0;
+inCorrect = 0;
+totalQuestions = 0;
+
+//function that on 'submit' maps over answers and if the value = correct +1 correct vice versa for inCorrect each +1 for totalQuestions
+function getValues() {
+
+  console.log("answer: " + answer)
+}
+function checkAnswers() {
+  console.log('button works')
+  getValues();
+}
+
