089efb285d0485ea4621177b4de5360c0e9c3a6c
Matthew Quade <quadem09@gmail.com>
2018-06-26 14:30:49 -0400

    changed up homepage, added links to routes

--- controllers/htmlRoutes.js
+++ controllers/htmlRoutes.js
@@ -5,7 +5,7 @@ var path = require('path');
 module.exports = function (app) {
 
   //HOME Page route
-  app.get("/home", function (req, res) {
+  app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/views/homePage.html"));
   })
   //Quiz1 route
